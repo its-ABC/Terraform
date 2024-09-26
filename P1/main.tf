4bc5a0a28ce7d3cf011b11e4752cf78c7f1d9995
its-ABC <albinbabu8800@gmail.com>
2024-09-26 23:43:55 +0530

    deleted unwanted files

--- P1/main.tf
+++
@@ -1,6 +0,0 @@
-
-resource "aws_instance" "my_ec2" {
-  ami = ""
-  instance_type = "t2.micro"
-}
-
--- P1/output.tf
+++
@@ -1,7 +0,0 @@
-
-output "private_ip" {
-    value = aws_instance.my_ec2.private_ip
-}
-output "public_ip" {
-    value = aws_instance.my_ec2.public_ip
-}
